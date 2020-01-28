726e9b4c019dfeefd635c49ef3960f547c85d251
Andrey <dvij@iMac.local>
2020-01-28 16:51:40 +0200

    1

--- Lesson3/task2/index.js
+++ Lesson3/task2/index.js
@@ -6,7 +6,7 @@ console.log('undefined to string is ' + String(undefined));
 console.log('0 to string is ' + String(0));
 
 console.log("'17' to number is " + Number('17'));
-console.log("'true' to number is " + Number(true));
+console.log("true to number is " + Number(true));
 console.log("'false' to number is " + Number('false'));
 console.log("'null' to number is " + Number('null'));
 console.log("'undefined' to number is " + Number('undefined'));
@@ -15,7 +15,7 @@ console.log("' ' to number is " + Number(' '));
 console.log("'30d' to number is " + Number('30d'));
 
 console.log('null to boolean is ' + Boolean(null));
-console.log('\'undefined\' to boolean is ' + Boolean('undefined'));
+console.log('undefined to boolean is ' + Boolean('undefined'));
 console.log('\'0\' to boolean is ' + Boolean('0'));
 console.log('\'-0\' to boolean is ' + Boolean('-0'));
 console.log('\'NaN\' to boolean is ' + Boolean(NaN));
