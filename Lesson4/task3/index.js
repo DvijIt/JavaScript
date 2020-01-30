e00b8fa43fe8650786550bbdcfa7c5723c63df75
Andrey <dvij@iMac.local>
2020-01-30 15:20:34 +0200

    1

--- Lesson4/task3/index.js
+++ Lesson4/task3/index.js
@@ -1,6 +1,16 @@
-for (let i = 1; i <= 5; i++) {
-  console.log('=> ' + i + ' <=')
+let i = 1;
+
+while (i <= 5 ) {
+  console.log('=> ' + i + ' <=');
   for (j = 1; j <= 10; j++) {
     console.log(i + ' x ' + j + ' = ' + i * j)
   }
+  i++;
 }
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log('=> ' + i + ' <=')
+//   for (j = 1; j <= 10; j++) {
+//     console.log(i + ' x ' + j + ' = ' + i * j)
+//   }
+// }
