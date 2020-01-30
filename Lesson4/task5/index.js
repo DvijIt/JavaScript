4717a192d7efd324754199459b1d9af19b493b97
Andrey <dvij@iMac.local>
2020-01-30 16:04:58 +0200

    1

--- Lesson4/task5/index.js
+++ Lesson4/task5/index.js
@@ -8,6 +8,14 @@ for (let i = m; i <= n; i++) {
     result += i;
   }
 
+  else if ( i % 3 == 0 ) {
+    result -= i;
+  }
+
+  else if ( i % 4 == 0 ) {
+    result *= i;
+  }
+
   else if ( i % 5 == 0 ) {
     console.log(i)
   }
