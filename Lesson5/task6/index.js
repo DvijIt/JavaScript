fb2b83d566daa5e166ff206d584c170378fcfb02
Andrey <dvij@iMac.local>
2020-01-30 19:50:34 +0200

    1

--- Lesson5/task6/index.js
+++ Lesson5/task6/index.js
@@ -1,7 +1,11 @@
 const getPrimes = num => {
-  for (let i = 1; i <= num; i++) {
+  let i = 1;
+  while (i <= num) {
     if (i % 1 === 0 && i % i === 0) {
       console.log(i);
+      i++;
+    } else {
+      i++;
     }
   }
 }
