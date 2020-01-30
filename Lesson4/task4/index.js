02ce6231d28893877047caec6a8c443b122b43cd
Andrey <dvij@iMac.local>
2020-01-30 15:09:16 +0200

    1

--- Lesson4/task4/index.js
+++ Lesson4/task4/index.js
@@ -1,7 +1,7 @@
 const n = 5;
-let sum = 1;
+let sum = '';
 
-for (i=2; i<=n; i++) {
+for (i=1; i<=n; i++) {
   sum += String(i);
 }
 console.log("Результат работы программы при n = "+ n + " это строка '" + sum +"'");
