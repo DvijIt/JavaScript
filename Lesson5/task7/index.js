cd953fd8ddea9e501ea12c3ef0195b867364d8f6
Andrey <dvij@iMac.local>
2020-01-30 19:10:40 +0200

    1

--- Lesson5/task7/index.js
+++ Lesson5/task7/index.js
@@ -1,11 +1,1 @@
-const increaser = (a, index) => {
-  let sum = 0;
-  if (a > index) {
-    sum = a * index;
-  } else {
-    sum = a;
-  }
-  return sum;
-}
-
-increaser(8, 6)
+const increaser = (a, index) => a > index ? a * index : a;
