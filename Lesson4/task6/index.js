a512913e700fc27e3e04f3cd5f6d9cd4bbce3e56
Andrey <dvij@iMac.local>
2020-01-30 15:47:27 +0200

    1

--- Lesson4/task6/index.js
+++ Lesson4/task6/index.js
@@ -3,10 +3,10 @@ for (i = 0; i <= 1000; i++) {
   if (i % 2 == 1) {
     console.log('Found');
     sum += i;
+    if ((sum * 5) > 5000) {
+      console.log('Bigger');
+    } else {
+      console.log('Smaller or equal')
+    }
   }
-}
-if ((sum * 5) > 5000) {
-  console.log('Bigger');
-} else {
-  console.log('Smaller or equal')
 }
