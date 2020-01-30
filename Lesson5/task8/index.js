9b669131ded1a57d25fce87a09e2c4cb9bd4d3a7
Andrey <dvij@iMac.local>
2020-01-30 19:36:50 +0200

    1

--- Lesson5/task8/index.js
+++ Lesson5/task8/index.js
@@ -1,14 +1,9 @@
-
-
 const compareSums = (a, b, c, d) => {
   let from = a + b;
   let to = c + d;
   if (from > to) {
-    return true
-  }
-  else {
-    return false
+    return true;
+  } else {
+    return false;
   }
 }
-
-compareSums(55, 2, 3, 4)
