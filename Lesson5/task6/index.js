e7614a3e0cced0df8645d1c7b7f0055b1f8d5b35
Andrey <dvij@iMac.local>
2020-01-30 20:40:04 +0200

    1

--- Lesson5/task6/index.js
+++ Lesson5/task6/index.js
@@ -1,12 +1,9 @@
 const getPrimes = num => {
-  nextPrime:
-  for (let i = 2; i <= num; i++) {
-  
+  nextPrime: for (let i = 2; i <= num; i++) {
     for (let j = 2; j < i; j++) {
       if (i % j == 0) continue nextPrime;
     }
-  
-    console.log( i );
+    console.log(i);
   }
 }
 getPrimes(5)
