9dbc7a31edbc07ade4c3e6edfc57ed340cfc6b4f
Andrey <dvij@iMac.local>
2020-02-01 19:02:28 +0200

    1

--- Lesson6/task7/index.js
+++ Lesson6/task7/index.js
@@ -1,17 +1,31 @@
 const array = [4, 5, 3, 6, 2, 1, 10];
 
 const sortAsc = array => {
-  arr = [];
-  for (; array.length;) {
-    arr.push(array.splice(array.indexOf(Math.min(...array)), 1)[0])
-  }
-  return arr;
+  array.sort((a, b) => b - a);
 }
 
-const sortDesc = array => {
-  arr = [];
-  for (; array.length;) {
-    arr.push(array.splice(array.indexOf(Math.max(...array)), 1)[0])
-  }
-  return arr;
+const sortAsc = array => {
+  array.sort((a, b) => a - b);
 }
+
+sortAsc(array);
+sortDesc(array);
+
+
+// const array = [4, 5, 3, 6, 2, 1, 10];
+
+// const sortAsc = array => {
+//   arr = [];
+//   for (; array.length;) {
+//     arr.push(array.splice(array.indexOf(Math.min(...array)), 1)[0])
+//   }
+//   return arr;
+// }
+
+// const sortDesc = array => {
+//   arr = [];
+//   for (; array.length;) {
+//     arr.push(array.splice(array.indexOf(Math.max(...array)), 1)[0])
+//   }
+//   return arr;
+// }
