c142b8dd5828c217933e5ae260cac247e02251c8
Andrey <dvij@iMac.local>
2020-02-01 19:06:00 +0200

    1

--- Lesson6/task7/index.js
+++ Lesson6/task7/index.js
@@ -1,11 +1,13 @@
 const array = [4, 5, 3, 6, 2, 1, 10];
 
 const sortAsc = array => {
-  array.sort((a, b) => b - a);
+  array.sort((a, b) => a - b);
+  return array;
 }
 
 const sortDesc = array => {
-  array.sort((a, b) => a - b);
+  array.sort((a, b) => b - a);
+  return array;
 }
 
 sortAsc(array);
