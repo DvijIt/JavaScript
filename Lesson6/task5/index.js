f95080031765a8921943f5a5c14c42e93d7d7bea
Andrey <dvij@iMac.local>
2020-02-01 13:02:46 +0200

    1

---
+++ Lesson6/task5/index.js
@@ -0,0 +1,12 @@
+const numbers = [11, 22, 33, 55, 66];
+
+function squareArray(numbers) {
+  let arr = [];
+  if (!Array.isArray(numbers)) {
+    return null;
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    arr.push(i * i);
+  }
+  return arr;
+}
