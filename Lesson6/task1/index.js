4f74a1fe5c9b7150bed056d3f18b96cf178b0c66
Andrey <dvij@iMac.local>
2020-02-01 09:33:39 +0200

    1

--- Lesson6/task1/index.js
+++ Lesson6/task1/index.js
@@ -2,5 +2,5 @@ function getArrayBounds(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
-  return [arr.lenght, arr[0], arr[arr.lenght - 1]];
+  return [arr.lenght, arr[1], arr[arr.lenght - 1]];
 }
