e81fbbeb41950c9bf2a5552be3cfa887ac0c5cc4
Andrey <dvij@iMac.local>
2020-02-01 14:49:45 +0200

    1

---
+++ Lesson6/task6/index.js
@@ -0,0 +1,10 @@
+const numbers = [11, 22, 33, 55, 66];
+
+function reverseArray(numbers) {
+  let arr = [];
+  if (!Array.isArray(numbers)) {
+    return null;
+  }
+  arr = numbers.reverse();
+  return arr;
+}
