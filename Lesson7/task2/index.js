b51c62bfdbde655f4321fadcc16c83e9ccbe76c9
Andrey <dvij@iMac.local>
2020-02-02 16:52:19 +0200

    1

--- Lesson7/task2/index.js
+++ Lesson7/task2/index.js
@@ -1,14 +1,4 @@
-const numbers = [11, 22, 33, 55, 66];
-
-function getSum(numbers) {
-  let sumOfNumbers = 0;
-  if (!Array.isArray(numbers)) {
-    return null;
-  }
-  for (let i = 0; i < numbers.length; i++) {
-    sumOfNumbers += numbers[i];
-  }
- 
-  return sumOfNumbers;
-  
+const sortDesc = array => {
+  array.sort((a, b) => b - a);
+  return array;
 }
