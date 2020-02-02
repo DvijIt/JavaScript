c5d1bafb0ba29ceca6716660f3d28d393bb41f15
Andrey <dvij@iMac.local>
2020-02-02 17:59:08 +0200

    1

--- Lesson7/task5/index.js
+++ Lesson7/task5/index.js
@@ -1,12 +1,1 @@
-const numbers = [11, 22, 33, 55, 66];
-
-function squareArray(numbers) {
-  let arr = [];
-  if (!Array.isArray(numbers)) {
-    return null;
-  }
-  for (let i = 0; i < numbers.length; i++) {
-    arr.push(Math.pow(numbers[i], 2));
-  }
-  return arr;
-}
+  const squareArray = arr => Array.isArray(arr) ? arr.map(el => Math.pow(el, 2)) : null
