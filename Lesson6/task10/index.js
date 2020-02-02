9444bda29d151414a3b4994021890e036fa82b0f
Andrey <dvij@iMac.local>
2020-02-02 09:57:46 +0200

    1

--- Lesson6/task10/index.js
+++ Lesson6/task10/index.js
@@ -1,7 +1,3 @@
 const clients = ['Tom', 'Jim', 'John', 'Bob'],
   balances = [1000, 2000, 3000, 4000],
-  withdraw = (clients, balances, client, amount) => {
-    let currentClient = clients.indexOf(client);
-    return balances[currentClient] - amount;
-  }
-withdraw(clients, balances, 'Bob', 2000);
+  withdraw = (clients, balances, client, amount) => balances[clients.indexOf(client)] - amount;
