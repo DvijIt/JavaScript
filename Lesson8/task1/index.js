0a6f469bf9b146c9ab6990ec99027180e9b930b6
Andrey <dvij@iMac.local>
2020-02-03 15:27:10 +0200

    1

--- Lesson8/task1/index.js
+++ Lesson8/task1/index.js
@@ -3,7 +3,11 @@ const transformToObject = arr => {
   arr.forEach(el => {
     obj[el] = el;
   });
+  if (arr.leght = 0) {
+    return obj;
+  }
   return obj;
 }
 
-const arr = ['a', 17.1, 'John Doe'];
+// const arr = ['a', 17.1, 'John Doe'];
+// console.log(transformToObject(arr))
