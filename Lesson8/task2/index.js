d41a50897e5f4cb9765d83c6ca9656aca64fc207
Andrey <dvij@iMac.local>
2020-02-03 15:45:03 +0200

    1

--- Lesson8/task2/index.js
+++ Lesson8/task2/index.js
@@ -1,5 +1,14 @@
-const sortDesc = array => {
-  const arr = [...array];
-  return arr.sort((a, b) => b - a);
+const concatProps = obj => {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
 }
-  
+// const obj = {
+//   name: 'John Doe',
+//   age: 17,
+//   interest: 'football'
+// }
+// const obj2 = {};
+// console.log(concatProps(obj2))
