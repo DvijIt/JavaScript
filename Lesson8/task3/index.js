905ba29a03937d6576092a16cc338aaf584ffb7e
Andrey <dvij@iMac.local>
2020-02-03 16:22:49 +0200

    1

--- Lesson8/task3/index.js
+++ Lesson8/task3/index.js
@@ -1,12 +1,20 @@
-const pickProps = (obj, arr) => {
-  obj1 = {};
-  arr.forEach(el => {
-    obj1[el] = obj[el]
-  });
-  return obj1;
-}
+  const pickProps = (obj, arr) => {
+    obj1 = {};
+    arr.forEach(el => {
+      for (let key in obj) {
+        if (key == el) {
+          obj1[el] = obj[key]
+        }
+      }
+    });
+    return obj1;
+  }
 
-// const obj$ = { a: 1, b: 2, c: 3 };
-// const array = ['a', 'c'];
+  // const obj$ = {
+  //   a: 1,
+  //   b: 2,
+  //   c: 3
+  // };
+  // const array = ['a', 'c'];
 
-// console.log(pickProps(obj$, array));
+  // console.log(pickProps(obj$, array));
