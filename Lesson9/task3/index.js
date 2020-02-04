5a4f77cf4d2b9392e8229bbc1bca81ce7134641c
Andrey <dvij@iMac.local>
2020-02-04 10:49:51 +0200

    1

--- Lesson9/task3/index.js
+++ Lesson9/task3/index.js
@@ -1,6 +1,20 @@
+const customers = {
+  'customer-id-1': {
+    name: 'William',
+    age: 54
+  },
+  'customer-id-2': {
+    name: 'Tom',
+    age: 17
+  }
+};
+
 const getCustomersList = usersObject => {
   const obj = Object.values(usersObject);
   obj.map( (el, index) => el.id = Object.keys(usersObject)[index]);
   obj.sort((a, b) => a.age - b.age);
   return obj;
 }
+
+// console.log(getCustomersList(customers));
+// console.log(customers);
