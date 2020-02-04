54f99336e438bd053b403d9721e8d06c46e6ee93
Andrey <dvij@iMac.local>
2020-02-04 11:25:40 +0200

    1

--- Lesson9/task4/index.js
+++ Lesson9/task4/index.js
@@ -1,9 +1,44 @@
-const getAdults = obj => {
-  let $obj = {};
-  for (let key in obj) {
-    if (obj[key] >= 18) {
-      $obj[key] = obj[key]
+const rooms = {
+  room1: [{
+      name: 'room1 name1'
+    },
+    {
+      name: 'room1 name2'
+    },
+    {
+      name: 'room1 name3'
+    },
+    {
+      name: 'room1 name4'
     }
-  }
-  return $obj;
+  ],
+  room2: [{
+    name: 'room2 name1'
+  }],
+  room3: [{
+      name: 'room3 name1'
+    },
+    {
+      name: 'room3 name2'
+    },
+    {
+      name: 'room3 name3'
+    }
+  ]
+};
+
+const getPeople = roomsObj => {
+  let arr = Object.values(roomsObj);
+  let $arr = [];
+  arr.forEach(el => {
+    el.forEach(el => {
+      el = Object.values(el)
+      $arr.push(el[0])
+    })
+  })
+  $arr.flat();
+
+  return $arr
 }
+
+console.log(getPeople(rooms))
