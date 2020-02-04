cf4348cb71b5f8f190d3be847005912abe8ea0ae
Andrey <dvij@iMac.local>
2020-02-04 11:26:12 +0200

    1

--- Lesson9/task4/index.js
+++ Lesson9/task4/index.js
@@ -1,31 +1,31 @@
-const rooms = {
-  room1: [{
-      name: 'room1 name1'
-    },
-    {
-      name: 'room1 name2'
-    },
-    {
-      name: 'room1 name3'
-    },
-    {
-      name: 'room1 name4'
-    }
-  ],
-  room2: [{
-    name: 'room2 name1'
-  }],
-  room3: [{
-      name: 'room3 name1'
-    },
-    {
-      name: 'room3 name2'
-    },
-    {
-      name: 'room3 name3'
-    }
-  ]
-};
+// const rooms = {
+//   room1: [{
+//       name: 'room1 name1'
+//     },
+//     {
+//       name: 'room1 name2'
+//     },
+//     {
+//       name: 'room1 name3'
+//     },
+//     {
+//       name: 'room1 name4'
+//     }
+//   ],
+//   room2: [{
+//     name: 'room2 name1'
+//   }],
+//   room3: [{
+//       name: 'room3 name1'
+//     },
+//     {
+//       name: 'room3 name2'
+//     },
+//     {
+//       name: 'room3 name3'
+//     }
+//   ]
+// };
 
 const getPeople = roomsObj => {
   let arr = Object.values(roomsObj);
@@ -41,4 +41,4 @@ const getPeople = roomsObj => {
   return $arr
 }
 
-console.log(getPeople(rooms))
+// console.log(getPeople(rooms))
