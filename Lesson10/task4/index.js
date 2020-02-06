860d3a96e0d035fd1236f5456b79cf9ce3ad90bc
Andrey <dvij@iMac.local>
2020-02-07 00:04:19 +0200

    1

--- Lesson10/task4/index.js
+++ Lesson10/task4/index.js
@@ -1,4 +1,8 @@
 const getMaxAbsoluteNumber = arr => {
-  const absoluteValue = arr.map(el => Math.abs(el));
-  return (!Array.isArray(arr)) || arr.length == 0 ? Math.max(...absoluteValue) : null
+  if ((!Array.isArray(arr)) || arr.length == 0) {
+    return null;
+  } else {
+    const absoluteValue = arr.map(el => Math.abs(el));
+    return Math.max(...absoluteValue)
+  }
 }
