0cb7003a30e5ed5bb4b0ae7500b8d0b0baefe598
Andrey <dvij@iMac.local>
2020-02-06 23:39:59 +0200

    1

--- Lesson10/task3/index.js
+++ Lesson10/task3/index.js
@@ -1,2 +1,2 @@
-const multiRound = num => [+Math.floor(num), +Math.round(num), +Math.ceil(num), +Math.trunc(num), +num.toFixed(3)]
-multiRound(-Math.PI)
+const multiRound = num => [+Math.floor(num), +Math.round(num), +Math.ceil(num), +Math.trunc(num), num.toFixed(3)]
+console.log(multiRound(-Math.PI))
