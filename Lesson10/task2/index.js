f52d226116f51bbc642ad8f6a18e11ed2534da81
Andrey <dvij@iMac.local>
2020-02-06 23:18:34 +0200

    1

--- Lesson10/task2/index.js
+++ Lesson10/task2/index.js
@@ -1,4 +1,5 @@
-const copyObj = obj => {
-  const $obj = { ...obj };
-  return $obj;
-}
+const getParsedIntegers = arr => arr.map(el => Number.parseInt(el));
+const getParsedIntegersV2 = arr => arr.map(el => parseInt(el));
+
+const getParsedFloats = arr => arr.map(el => Number.parseFloat(el));
+const getParsedFloatsV2 = arr => arr.map(el => parseFloat(el));
