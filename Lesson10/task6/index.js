2a673df9c3f311cb255ab959a5c5ec70f5248784
Andrey <dvij@iMac.local>
2020-02-07 09:16:34 +0200

    1

--- Lesson10/task6/index.js
+++ Lesson10/task6/index.js
@@ -3,8 +3,11 @@ const getRandomNumbers = (numLength, start, end) => {
     return null;
   }
   let arr = [];
+  start = Math.ceil(start);
+  end = Math.floor(end);
+  
   for (let i = 0; i < numLength; i++) {
-    arr[i] = Math.round(Math.random() * (start - end + 1)) + end;
+    arr[i] = Math.floor(Math.random() * (start - end + 1)) + end; //Максимум и минимум включаются
   }
   console.log(arr.length)
   return arr;
