686c881c5b2fcbceda9319deb09bb110260bf84e
Andrey <dvij@iMac.local>
2020-02-07 09:59:06 +0200

    1

---
+++ Lesson10/task7/index.js
@@ -0,0 +1,7 @@
+const superRound = (num, n) => [
+  +Math.floor(num * +Math.pow(10, n)) / +Math.pow(10, n),
+  +Math.round(num * +Math.pow(10, n)) / +Math.pow(10, n),
+  +Math.ceil(num * +Math.pow(10, n)) / +Math.pow(10, n),
+  +Math.trunc(num * +Math.pow(10, n)) / +Math.pow(10, n),
+  +num.toFixed(n)
+];
