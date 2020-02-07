92c770f5eb9de1255ab2ef784fe09dc2e7237fb3
Andrey <dvij@iMac.local>
2020-02-07 09:12:16 +0200

    1

--- Lesson10/task6/index.js
+++ Lesson10/task6/index.js
@@ -1,8 +1,12 @@
 const getRandomNumbers = (numLength, start, end) => {
+  if (start > end) {
+    return null;
+  }
   let arr = [];
-  for (let i = 0; i <= numLength; i++) {
+  for (let i = 0; i < numLength; i++) {
     arr[i] = Math.round(Math.random() * (start - end + 1)) + end;
   }
+  console.log(arr.length)
   return arr;
 }
 
