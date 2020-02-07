1b0fe6ae025e7072c4bf5a80826b35efe63b1c59
Andrey <dvij@iMac.local>
2020-02-07 13:09:17 +0200

    1

--- Lesson10/task6/index.js
+++ Lesson10/task6/index.js
@@ -7,12 +7,14 @@ const getRandomNumbers = (numLength, start, end) => {
   end = Math.floor(end);
 
   for (let i = 0; i < numLength; i++) {
-    arr[i] = Math.random() * (start - end + 1) + end; //Максимум и минимум включаются
+    arr[i] = +Math.round(Math.random() * (start - end + 1)) + end; //Максимум и минимум включаются
   }
   if ((arr.find(el => Number.isInteger(el))) === undefined) {
     return null
+  }else {
+    return arr;
   }
-  return arr;
+  
 }
 
 console.log(getRandomNumbers(5, -5, 11))
