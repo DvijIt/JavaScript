c6bc68791cd45d818788e96e5a626cb50179425b
Andrey <dvij@iMac.local>
2020-02-08 22:12:55 +0200

    1

--- Lesson11/task4/index.js
+++ Lesson11/task4/index.js
@@ -1,44 +1,6 @@
-// const rooms = {
-//   room1: [{
-//       name: 'room1 name1'
-//     },
-//     {
-//       name: 'room1 name2'
-//     },
-//     {
-//       name: 'room1 name3'
-//     },
-//     {
-//       name: 'room1 name4'
-//     }
-//   ],
-//   room2: [{
-//     name: 'room2 name1'
-//   }],
-//   room3: [{
-//       name: 'room3 name1'
-//     },
-//     {
-//       name: 'room3 name2'
-//     },
-//     {
-//       name: 'room3 name3'
-//     }
-//   ]
-// };
+const countOccurrences = (fullStr, subStr) =>  typeof(subStr) === 'string' ? fullStr.match(RegExp(subStr, 'g')).length : null;
 
-const getPeople = roomsObj => {
-  let arr = Object.values(roomsObj);
-  let $arr = [];
-  arr.forEach(el => {
-    el.forEach(el => {
-      el = Object.values(el)
-      $arr.push(el[0])
-    })
-  })
-  $arr.flat();
+// const someText = 'text text text';
+// const subStr = 'te';
 
-  return $arr
-}
-
-// console.log(getPeople(rooms))
+// console.log(countOccurrences(someText, subStr));
