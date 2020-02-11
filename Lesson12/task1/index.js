47518aea0c70e1312fd674dc642676529ebcc794
Andrey <dvij@iMac.local>
2020-02-11 11:32:31 +0200

    1

--- Lesson12/task1/index.js
+++ Lesson12/task1/index.js
@@ -1,19 +1,27 @@
 const calc = expression => {
-  if (typeof(expression) != 'string') {
+  if (typeof (expression) != 'string') {
     return null;
   }
-  const [a , operator, b]  = expression.split(' ');
+  const [a, operator, b] = expression.split(' ');
   let result;
   switch (operator) {
     case '+':
       result = +a + +b;
       break;
-  
+
     case '-':
       result = +a - +b;
+      break;
+
+    case '*':
+      result = +a / +b;
+      break;
+
+    case '/':
+      result = +a / +b;
       break;
   }
   return `${expression} = ${result}`;
 }
 
-console.log(calc('1 - 2'))
+// console.log(calc('1 / 2'))
