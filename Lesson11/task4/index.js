6a91cac9320209c2e2c71aaf23d23b9a07a584cf
Andrey <dvij@iMac.local>
2020-02-11 11:31:18 +0200

    1

--- Lesson11/task4/index.js
+++ Lesson11/task4/index.js
@@ -1,6 +1,1 @@
 const countOccurrences = (fullStr, subStr) =>  subStr.length > 0 ? fullStr.match(RegExp(subStr, 'g')).length : null;
-
-// const someText = 'text text text';
-// const subStr = 'te';
-
-// console.log(countOccurrences(someText, subStr));
