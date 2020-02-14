8096bfe6b55a4c0aa4a4a84288040835d00511ba
Andrey <dvij@iMac.local>
2020-02-14 11:48:41 +0200

    1

--- Lesson14/task2/index.js
+++ Lesson14/task2/index.js
@@ -1,10 +1,10 @@
-export default function createMessenger() {
-  let message = 'just learn it';
+function createMessenger() {
+  let message = 'Just learn it!';
 
   let sender = 'Gromcode';
   
   function sendMessage(name) {
-    console.log(`${name}, ${message}! Your ${sender}`);
+    console.log(`${name}, ${message} Your ${sender}`);
   }
   
   function setMessage(text) {
@@ -20,7 +20,14 @@ export default function createMessenger() {
     setSender
   };
 }
-const messenger1 = createMessenger();
-messenger1.setMessage('Good job!');
-messenger1.setSender('Gromcode')
-messenger1.sendMessage('Bob');
+// const messenger1 = createMessenger();
+// messenger1.sendMessage('Bob');
+// messenger1.setMessage('Good job!');
+// messenger1.setSender('Gromcode');
+// messenger1.sendMessage('Bob');
+
+// const messenger2 = createMessenger();
+// messenger2.sendMessage('Ann');
+// messenger2.setMessage('Good!');
+// messenger2.setSender('Gromcode2');
+// messenger2.sendMessage('Ann');
