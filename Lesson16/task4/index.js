dc0750964548441480463146fabbc7392de42719
Andrey <dvij@iMac.local>
2020-02-18 21:35:26 +0200

    1

--- Lesson16/task4/index.js
+++ Lesson16/task4/index.js
@@ -1,11 +1,13 @@
-let foo = 1; 
+var foo = 1;
 
-function bar() { 
-    if (!foo) { 
-        let foo = 10; 
-    } 
+function bar() {
+    if (!foo) {
+        let foo = 10;
+    }
 
     return foo;
 }
+
+var foo = bar();
 
 export default foo;
