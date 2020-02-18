85d2f6599d923db4dede79be0286b7d31001f110
Andrey <dvij@iMac.local>
2020-02-18 21:25:57 +0200

    1

--- Lesson16/task3/index.js
+++ Lesson16/task3/index.js
@@ -2,7 +2,9 @@ export function createArrayOfFunctions(num) {
   let arr = [];
   if (Number.isInteger(num)) {
     for (let i = 0; i <= num; i++) {
-      arr[i] = () => i;
+      arr[i] = function() {
+        return i;
+      }
     }
   } else if (num === undefined) {
     return arr;
