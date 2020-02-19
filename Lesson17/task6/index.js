58e2b89d1a01f38b542b7e11630d3dc3ce35f286
Andrey <dvij@iMac.local>
2020-02-19 15:59:05 +0200

    1

---
+++ Lesson17/task6/index.js
@@ -0,0 +1,7 @@
+function bind(func, context) {
+    let bindArgs = [].slice.call(args);
+    return function() {
+        let funcArgs = [].slice.call(args);
+        return func.apply(context, bindArgs.concat(funcArgs));
+    };
+};
