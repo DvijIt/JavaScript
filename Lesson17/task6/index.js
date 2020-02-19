7cdafb7b9ad169888615b7dd7a15df10ebc0b2c2
Andrey <dvij@iMac.local>
2020-02-19 16:00:33 +0200

    1

--- Lesson17/task6/index.js
+++ Lesson17/task6/index.js
@@ -1,5 +1,5 @@
 export function bind(func, context) {
-    let bindArgs = [].slice.call(args);
+    let bindArgs = [].slice.call(args, 2);
     return function() {
         let funcArgs = [].slice.call(args);
         return func.apply(context, bindArgs.concat(funcArgs));
