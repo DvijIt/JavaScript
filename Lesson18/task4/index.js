afe0d45bc00acbcdfb550564418cb2ee653c16a7
Andrey <dvij@iMac.local>
2020-02-20 19:41:11 +0200

    1

--- Lesson18/task4/index.js
+++ Lesson18/task4/index.js
@@ -1,9 +1,12 @@
-function spy(test) {
-    function spyFunk() {
-        styFunk.calls.push([...arguments])
-        return test.apply(this, arguments)
-    }
-    spyF.calls = [];
-    return spyFunk;
-};
+function spy(func) {
+  function spyF() {
+    spyF.calls.push([...arguments]);
+    return func.apply(this, arguments);
+  }
+
+  spyF.calls = [];
+
+  return spyF;
+}
+
 export { spy };
