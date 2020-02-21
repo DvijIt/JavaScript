da993ab89b2a99bf7b02ffc2692642e4ca8600ea
Andrey <dvij@iMac.local>
2020-02-21 14:18:07 +0200

    1

--- Lesson20/task4/index.js
+++ Lesson20/task4/index.js
@@ -1,28 +1,29 @@
 class Order {
-  id = toString(Math.random())
-  price = 0
-  dateCreated = new Date()
-  isConfirmed = false
-  dateConfirmed = new Date()
-  city = ''
-  type = 'type'
+  id = toString(Math.random());
+  price = 0;
+  dateCreated = new Date();
+  isConfirmed = false;
+  dateConfirmed = new Date();
+  city = "";
+  type = "type";
   constructor(price, city, type, ID) {
-    this.price = price
-    this.city = city
-    this.type = type
-    this.id = ID
+    this.price = price;
+    this.city = city;
+    this.type = type;
+    this.id = ID;
   }
 
   checkPrice() {
-    return this.price > 1000
+    return this.price > 1000;
   }
 
   confirmOrder() {
-    this.isConfirmed = true
-    this.dateConfirmed = new Date()
+    this.isConfirmed = true;
+    this.dateConfirmed = new Date();
   }
 
   isValidType() {
-    if (this.type === 'Buy' || this.type === 'Sell') return true;
+    if (this.type === "Buy" || this.type === "Sell") return true;
   }
 }
+export { Order };
