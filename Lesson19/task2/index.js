b3432cf473d7cce03c7518d67ba27dbb5b0fc158
Andrey <dvij@iMac.local>
2020-02-21 09:19:54 +0200

    1

--- Lesson19/task2/index.js
+++ Lesson19/task2/index.js
@@ -1,6 +1,6 @@
 function getOwnProps(obj) {
   const objProps = [];
-  for (prop in obj) {
+  for (let prop in obj) {
     if (obj.hasOwnProperty(prop) && typeof obj[prop] !== "function") {
       objProps.push(prop);
     }
