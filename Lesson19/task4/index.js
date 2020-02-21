c0acec55fb154a1a8c0287760f373db2b96b34e3
Andrey <dvij@iMac.local>
2020-02-21 09:48:40 +0200

    1

--- Lesson19/task4/index.js
+++ Lesson19/task4/index.js
@@ -1,9 +1,24 @@
-function spy(test) {
-  function spyFunk() {
-    spyFunk.calls.push([...arguments]);
-    return test.apply(this, arguments);
-  }
-  spyFunk.calls = [];
-  return spyFunk;
+function User(name, age) {
+  this.name = name;
+  this.age = age;
 }
-export { spy };
+
+User.prototype.sayHi = function() {
+  console.log(`Hi, I am ${this.name}`);
+};
+User.prototype.requestNewPhoto = function() {
+  console.log(`New photo request was sent for ${this.name}`);
+};
+User.prototype.setAge = function(age) {
+  if (age >= 25) {
+    this.age = age;
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  if (age < 0) {
+    return false;
+  }
+  if (age > 0 && age < 25) {
+    return age;
+  }
+};
+export { User }
