ed354b91d7031b581cd631a34addbfc5e74b1837
Andrey <dvij@iMac.local>
2020-02-21 12:52:43 +0200

    1

--- Lesson20/task2/index.js
+++ Lesson20/task2/index.js
@@ -1,22 +1,22 @@
-const vehicle = {
+class Vehicle {
   move() {
     console.log(`${this.name} is moving`)
-  },
+  }
   stop() {
     console.log(`${this.name} stopped`)
   }
 }
 
-const ship = {
+class Ship {
   name: 'Argo',
   startMachine() {
     console.log(`${this.name} lifting anchor up`);
     this.move();
-  },
+  }
   stopMachine() {
     this.stop();
     console.log(`${this.name} lifting anchor down`);
-  },
+  }
   __proto__: vehicle
 }
 
