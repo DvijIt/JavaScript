7344e735d148e9c20f30ab6cd5a68b7fd7b3af8c
Andrey <dvij@iMac.local>
2020-02-25 11:11:42 +0200

    1

--- Lesson24/task4/index.js
+++ Lesson24/task4/index.js
@@ -1,4 +1,4 @@
-students = [
+const students = [
   { name: "Tom", birthDate: "01/15/2010" },
   { name: "Ben", birthDate: "01/17/2008" },
   { name: "Sam", birthDate: "03/15/2010" },
@@ -11,17 +11,17 @@ const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'O
 const studentsBirthDays = students => {
   const studentsArr = students.slice('')
 
-  const result = {}
+  const monthBirthDay = {}
 
   month.forEach(item => {
-    result[item] = [];
+    monthBirthDay[item] = [];
   });
   studentsArr.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
   .map(item => {
     const monthsName = month[new Date(item.birthDate).getMonth()];
-    result[monthsName].push(item.name);
+    monthBirthDay[monthsName].push(item.name);
   });
-  Object.keys(result).filter(item => result[item].length > 0 ? true : delete result[item])
-  return result
+  Object.keys(monthBirthDay).filter(item => monthBirthDay[item].length > 0 ? true : delete monthBirthDay[item])
+  return monthBirthDay
 };
 export { studentsBirthDays };
