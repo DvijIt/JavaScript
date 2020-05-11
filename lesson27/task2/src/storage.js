04eafb5018602122ccd97504bddea9ef50d779b7
Andrey <dvij@iMac.local>
2020-05-11 21:17:25 +0300

    1

--- lesson27/task2/src/storage.js
+++ lesson27/task2/src/storage.js
@@ -1,11 +1,3 @@
-export const tasks = [
-  { text: "Buy milk", done: false, date: 'Mon May 11 2020 18:21:50 GMT+0300 (Восточная Европа, летнее время' },
-  { text: "Pick up Tom from airport", done: false, date: 'Mon May 11 2020 18:22:50 GMT+0300 (Восточная Европа, летнее время' },
-  { text: "Visit party", done: false, date: 'Mon May 11 2020 18:23:50 GMT+0300 (Восточная Европа, летнее время' },
-  { text: "Visit doctor", done: true, date: 'Mon May 11 2020 18:24:50 GMT+0300 (Восточная Европа, летнее время' },
-  { text: "Buy meat", done: true, date: 'Mon May 11 2020 18:25:50 GMT+0300 (Восточная Европа, летнее время' }
-];
-
 
 export const setItem = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
