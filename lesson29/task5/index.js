829229d064fa682af609563508d195fb287989b6
Andrey <dvij@iMac.local>
2020-05-14 19:49:09 +0300

    upload

--- lesson29/task5/index.js
+++ lesson29/task5/index.js
@@ -1,4 +1,4 @@
-const delay = (delay, callback, context, arg1, arg2, arg3) => {
+export const delay = (delay, callback, context, arg1, arg2, arg3) => {
     setTimeout(() => {
         callback(context)
     }, delay)
