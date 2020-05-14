a9138080369db990cb9bb891f7bb2e28c9c7a10b
Andrey <dvij@iMac.local>
2020-05-14 20:58:13 +0300

    promisification

--- lesson30/task5/index.js
+++ lesson30/task5/index.js
@@ -1,5 +1,51 @@
-export const delay = (delay, callback, context, ...args) => {
-    setTimeout(() => {
-        callback.call(context, ...args)
-    }, delay)
-}
+// export const addImage = (url, callback) => {
+//     const img = document.createElement('img');
+//     img.setAttribute('alt', 'User avatar');
+//     img.src = url;
+
+
+//     const pageElem = document.querySelector('.page');
+//     pageElem.append(img);
+    
+//     const onImageLoaded = () => {
+//         const { width, height } = img;
+//         callback(null, { width, height });
+//     }
+    
+//     const onImageLoadError = () => callback('Image load failed');
+    
+//     img.addEventListener('load', onImageLoaded);
+    
+//     img.addEventListener('error', onImageLoadError);
+// };
+
+export const addImageV2 = (url) => {
+    let p = new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        img.setAttribute('alt', 'User avatar');
+        img.src = url;
+    
+        const pageElem = document.querySelector('.page');
+        pageElem.append(img);
+        
+        const onImageLoaded = () => {
+            const { width, height } = img;
+            resolve({ width, height });
+        }
+        
+        const onImageLoadError = () => reject(new Error('Image load failed'));
+        
+        img.addEventListener('load', onImageLoaded);
+        
+        img.addEventListener('error', onImageLoadError);
+    })
+
+    return p;
+};
+
+let url = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+
+let result = addImageV2(url)
+result
+    .then(data => console.log(data)) // { width: 200, height: 100 }
+    .catch(error => console.log(error)); // 'Image load failed'
