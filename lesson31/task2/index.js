7a2e943ea2462e4245c20d69f116d87ee15ac46d
Andrey <dvij@iMac.local>
2020-05-17 22:53:02 +0300

    lesson 31 task 2

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -1,38 +1,21 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise(resolve => {
+const asyncCalculator = num => new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ name: 'Tom', age: 17 });
-    }, 1000);
-  });
-  
-  /*
-   * Выведите в консоль переменную userDataPromise
-   * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
-   */
-  
-  /* ...code here */
-  console.log(userDataPromise);
-  
-  
-  /*
-   * Выведите в консоль переменную userData в обработчике промиса
-   * Ответьте себе на вопрос, что находится в переменной userData?
-   */
-  userDataPromise
-    .then(function onSuccess(userData) {
-      /* ...code here */
-      console.log(userData);
-    });
-  
-  /*
-   * подпишитесь на успешное выполнение промиса userDataPromise
-   * используйте метод .then
-   * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
-   * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
-   */
-  
-  userDataPromise
-    .then(function onSuccess({name, age}) {
-      /* ...code here */
-      console.log(`My name is ${name}. I am ${age} years old`);
-    });
+      console.log(`Initial value: ${num}`)
+      resolve(num)
+    }, 500)
+  })
+  .then(value => new Promise((resolve) => {
+    setTimeout(() => {
+      const result = value * value
+      console.log(`Squared value: ${result}`)
+      resolve(result)
+    }, 500)
+  }))
+  .then(value => {
+    const result = value * 2
+    console.log(`Doublet value: ${result}`)
+    return result
+  })
+
+
+asyncCalculator(5)
