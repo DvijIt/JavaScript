d631fa1f382efadd28451b232482a97a11bf78ad
Andrey <dvij@iMac.local>
2020-05-18 10:52:45 +0300

    lesson 32 task 2

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -1,23 +1,32 @@
- export const asyncCalculator = num => new Promise((resolve) => {
-     setTimeout(() => {
-       console.log(`Initial value: ${num}`)
-       resolve(num)
-     }, 500)
-   })
-   .then(value => new Promise((resolve) => {
-     setTimeout(() => {
-       const result = value * value
-       console.log(`Squared value: ${result}`)
-       resolve(result)
-     }, 500)
-   }))
-   .then(value => new Promise((resolve) => {
-     setTimeout(() => {
-       const result = value * 2;
-       console.log(`Doubled value: ${result}`);
-       resolve(result);
-     }, 500);
-   }))
+const getRandomNumber = (from, to) => from + Math.random() * (to - from)
 
+const request = url => new Promise(resolve => {
+  const randomDelay = getRandomNumber(1000, 3000)
+  setTimeout(() => {
+    resolve({
+      userData: {
+        name: 'Tom',
+        age: 17
+      },
+      source: url
+    })
+  }, randomDelay);
+})
 
- asyncCalculator(5)
+const servers = [
+  'https://server.com/us/userId',
+  'https://server.com/eu/userId',
+  'https://server.com/au/userId'
+]
+
+export const getUserAPAP = userId => {
+  const userUrls = servers
+    .map(serverUrl => `${serverUrl}/users/${userId}`)
+  const requests = userUrls
+    .map(userUrl => request(userUrl))
+
+  return Promise.race(requests);
+}
+
+// getUserAPAP('user-id-1')
+//   .then(res => console.log(res))
