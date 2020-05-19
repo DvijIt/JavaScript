846bcf6cf97c5603c95c651715a8d4aea5a37c41
Andrey <dvij@iMac.local>
2020-05-19 19:15:11 +0300

    update fix

--- Lesson34/task2/index.js
+++ Lesson34/task2/index.js
@@ -7,25 +7,38 @@ export const getUsersList = () => {
 export const getUserById = userId => {
   return fetch(`${baseUrl}/${userId}`).then(responce => responce.json());
 }
-
+const newUserData = {
+  email: 'cool@email.com',
+  firstName: 'Iron',
+  lastName: 'Man',
+  age: 42,
+};
 export const createUser = userData => {
+  userData = newUserData;
   return fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(userData)
-  }).then(responce => responce.json());
+  })
 }
 
+const updatedUserData = {
+  email: 'new@email.com',
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 17,
+};
 export const updateUser = (userId, updateUserData) => {
+  updateUserData = updatedUserData;
   return fetch(`${baseUrl}/${userId}`, {
     method: 'PUT',
     headers: {
       'Content-type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(updateUserData)
-  }).then(responce => responce.json());
+  })
 }
 
 export const deleteUser = userId => {
