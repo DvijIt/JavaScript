0f6b79597f9d4ee2545174a493ceeeaa274c6b35
Andrey <dvij@iMac.local>
2020-05-19 18:35:08 +0300

    todoList with REST_API

--- lesson33/task3/index.js
+++ lesson33/task3/index.js
@@ -1,13 +1,40 @@
-/*
- * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
- */
 
-/* ...code here */
-export const makePromise = num => Promise.resolve(num)
-/*
- * пример использования
- */
-makePromise(17)
-    .then(number => {
-        console.log(number); // 17
-    });
+const commit = (userId, repoId) => {
+    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+        .then(response => response.json())
+}
+
+
+export function getMostActiveDevs({ days, userId, repoId }) {
+    return commit(userId, repoId)
+        .then(commitData => {
+
+            const commitCount = countCommit(commitData, days)
+                .sort((a, b) => b.count - a.count)
+
+            return commitCount
+                .filter(({ count }) => count === commitCount[0].count)
+        })
+}
+
+
+const countCommit = (data, daysPast) => {
+    const start = new Date().setDate(new Date().getDate() - daysPast)
+    const startToEnd = data
+        .map(({ commit: { author } }) => author)
+        .filter(({ date }) => new Date(date).getTime() >= start)
+
+    const uniq = new Set(startToEnd.map(({ email }) => email))
+    return [...uniq]
+        .map(commitEmail => {
+            const comment = startToEnd
+                .filter(({ email }) => email === commitEmail)
+            return {
+                count: comment.length,
+                name: comment[0].name,
+                email: commitEmail,
+            }
+        })
+}
+
+getMostActiveDevs({ days: 16, userId: 'facebook', repoId: 'react' }).then(data => console.log(data))
