5ea2a23d0e2b2be7e24bff4a4ed25d1879e5d3ad
Andrey <dvij@iMac.local>
2020-05-19 18:59:28 +0300

    update fix

--- Lesson34/task2/index.js
+++ Lesson34/task2/index.js
@@ -1,8 +1,12 @@
 const baseUrl = 'https://5ec377e28ebdcc0016a5a8e4.mockapi.io/api/v1/users'
 
-const getUsersList = () => fetch(baseUrl).then(responce => responce.json());
+const getUsersList = () => {
+  return fetch(baseUrl).then(responce => responce.json());
+}
 
-const getUserById = userId => fetch(`${baseUrl}/${userId}`).then(responce => responce.json())
+const getUserById = userId => {
+  return fetch(`${baseUrl}/${userId}`).then(responce => responce.json());
+}
 
 const createUser = userData => {
   return fetch(baseUrl, {
@@ -14,40 +18,19 @@ const createUser = userData => {
   })
 }
 
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
+const updateUser = (userId, updateUserData) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(updateUserData)
+  })
+}
 
 const deleteUser = userId => {
   return fetch(`${baseUrl}/${userId}`, {
     method: 'DELETE',
   });
 }
-
-// const updateUser = (userId, updateUserData) => {
-//   return fetch(`${baseUrl}/${userId}`, {
-//     method: 'PUT',
-//     headers: {
-//       'Content-type': 'application/json;charset=utf-8'
-//     },
-//     body: JSON.stringify(updateUserData)
-//   })
-// }
-// const userUpdateData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
-
-
-
-
-
-
-
-
-      
+    
