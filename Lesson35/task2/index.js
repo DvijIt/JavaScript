3a0e5f3d63b2cc8085c11e2ec267cd22e5d8e362
Andrey <dvij@iMac.local>
2020-05-20 18:10:16 +0300

    lesson 35 task1

---
+++ Lesson35/task2/index.js
@@ -0,0 +1,36 @@
+const baseUrl = 'https://5ec377e28ebdcc0016a5a8e4.mockapi.io/api/v1/users'
+
+export const getUsersList = () => {
+  return fetch(baseUrl).then(responce => responce.json());
+}
+
+export const getUserById = userId => {
+  return fetch(`${baseUrl}/${userId}`).then(responce => responce.json());
+}
+
+export const createUser = newUserData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(newUserData)
+  })
+}
+
+export const updateUser = (userId, updatedUserData) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(updatedUserData)
+  })
+}
+
+export const deleteUser = userId => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+  });
+}
+    
