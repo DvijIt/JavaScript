7ebcf6889ea618086be83892290486be017bef75
Andrey <dvij@iMac.local>
2020-05-20 22:34:13 +0300

    async getUser

---
+++ Lesson36/task1/index.js
@@ -0,0 +1,16 @@
+export const getUser = async userId => {
+  try {
+    const responce = await fetch(`https://api.github.com/users/${userId}`);
+    if (!responce.ok) {
+      return null
+    }
+    const userData = await responce.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to fetch user')
+  }
+};
+
+getUser('facebook')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err.message));
