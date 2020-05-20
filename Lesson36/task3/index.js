6ba3bb2698bfebc8be1d03417f8f34fe19bb7cd2
Andrey <dvij@iMac.local>
2020-05-20 23:04:45 +0300

    async / await userDataList email

--- Lesson36/task3/index.js
+++ Lesson36/task3/index.js
@@ -1,24 +1,12 @@
-const successRequest = Promise.resolve({ name: "Tom" });
+export const getUsersBlogs = async usersList => {
+    const requests = usersList
+        .map(userId => fetch(`https://api.github.com/users/${userId}`)
+            .then(responce => {
+                if (responce.ok) responce.json();
+                throw new Error('Failed to load data');
+            }));
+    const usersData = await Promise.all(requests);
+    const usersBlogs = usersData.map(user => user.blog);
+    return usersBlogs
 
-successRequest
-    .then(function onSuccess1(data) {
-        throw new Error('Error with data')
-    })
-    .catch(function onError1(error) {
-        console.error("onError1", error.message);
-    })
-
-
-const failRequest = Promise.reject(new Error("Something went wrong"));
-
-failRequest
-    .catch(function onError2(error) {
-        console.error("onError2", error.message);
-        throw new Error('Server error');
-    })
-    .then(function onSuccess2(data) {
-        console.log("onSuccess2", data);
-    })
-    .catch(function onError3(error) {
-        console.error("onError3", error.message);
-    });
+}
