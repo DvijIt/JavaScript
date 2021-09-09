768155358d8bdca40156e1f0d57bb4de369b1c5e
DvijIt <andreydnb@ukr.net>
2021-09-09 16:10:13 +0300

    qwe

---
+++ Lesson21/hw9/index.js
@@ -0,0 +1,11 @@
+export const finishForm = () => {
+  const formElem = document.querySelector('.login-form');
+
+  const loginInputElem = document.createElement('input');
+  loginInputElem.setAttribute('type', 'text');
+  loginInputElem.setAttribute('name', 'login');
+  formElem.append(loginInputElem);
+
+  const passwordInputElem = document.querySelector('input[name="password"]');
+  passwordInputElem.setAttribute('type', 'password');
+};
